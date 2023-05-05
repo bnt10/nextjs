886aede3d35768de6ce1270baa48b1cda040a856
jinhyeong.kim@snetsystems.co.kr <jinhyeong.kim@snetsystems.co.kr>
2023-05-05 16:14:58 +0900

    feat: add login page wellcom text

--- src/component/OAuthLogin/GoogleLoginButton.tsx
+++ src/component/OAuthLogin/GoogleLoginButton.tsx
@@ -1,9 +1,11 @@
 import Button from '@/component/common/Button'
+import GoogleLoginIcon from '@/public/assets/icons/google-login-Icon.svg'
 
 const GoogleLoginButton = () => {
   const handler = () => {}
   return (
     <li className="flex list-none justify-center">
+      <GoogleLoginIcon className="absolute left-7pxr top-5pxr h-30pxr w-30pxr object-cover" />
       <Button
         className={'text-center '}
         title={'Google Login'}
--- src/component/OAuthLogin/index.tsx
+++ src/component/OAuthLogin/index.tsx
@@ -1,40 +1,37 @@
 import Image from 'next/image'
 
 import logoBg from '@/public/assets/images/app-login-bg.png'
-import type { ProviderConfig } from '@/types/oauthProvider'
-
-import HText from '../common/HText'
-import GitHubLoginButton from './GitHubLoginButton'
-import GoogleLoginButton from './GoogleLoginButton'
 
 const AuthLogin = () => {
-  const LoginOAuth: Record<string, ProviderConfig> = {
-    google: {
-      id: 'googleLogin',
-      component: GoogleLoginButton,
-    },
-    github: {
-      id: 'githubLogin',
-      component: GitHubLoginButton,
-    },
-  }
+  // const LoginOAuth: Record<string, ProviderConfig> = {
+  //   google: {
+  //     id: 'googleLogin',
+  //     component: GoogleLoginButton,
+  //   },
+  // }
 
   return (
     <>
-      <div>
+      <div className="relative flex h-screen flex-col items-center ">
         <Image
           src={logoBg}
           alt="login-bg"
-          className="absolute top-0pxr -z-10 h-full w-full bg-gray-900 object-cover"
+          className="absolute top-[-32px]  z-[-10] w-390pxr bg-gray-900 object-cover"
         />
-        <HText level={1} text={'wellcome to'} />
-        <div>
+        <p className="relative mt-56pxr text-left text-lg text-white">
+          Welcome to
+        </p>
+        <p className="relative mt-107pxr h-37pxr w-248pxr text-left text-4xl font-bold text-white">
+          Wild Alliance
+        </p>
+
+        {/* <div>
           {Object.values(LoginOAuth).map(
             ({ id, component: OAuthLoginButton }) => (
               <OAuthLoginButton key={id} />
             )
           )}
-        </div>
+        </div> */}
       </div>
     </>
   )
