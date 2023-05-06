823d48d8a5b213410f54ba744c391d8619aad424
jinhyeong.kim@snetsystems.co.kr <jinhyeong.kim@snetsystems.co.kr>
2023-05-06 14:20:12 +0900

    feat: add twMergeObjects method for merging objects
    Add twMergeObjects function for merging objects to improve code
    reusability and reduce redundancy

---
+++ src/utils/twObjectMerged.ts
@@ -0,0 +1,26 @@
+import { twMerge } from 'tailwind-merge'
+
+const tw = <T extends Record<string, string>>(
+  originStyle: T,
+  customStyle?: T
+): T => {
+  if (!customStyle || typeof customStyle === 'string') {
+    return customStyle as unknown as T
+  }
+
+  const mergedStyle = (
+    Object.entries(originStyle) as [keyof T, string][]
+  ).reduce((acc: Partial<T>, [key, baseStyle]) => {
+    const customStyleValue = customStyle[key]
+    if (customStyleValue) {
+      acc[key] = twMerge(baseStyle, customStyleValue) as T[keyof T]
+    } else {
+      acc[key] = baseStyle as T[keyof T]
+    }
+    return acc
+  }, {})
+
+  return { ...originStyle, ...mergedStyle } as T
+}
+
+export default tw
