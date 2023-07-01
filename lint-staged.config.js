589db4f9f329e562aeaa76db42bc7edd473338d4
jin <bnt10@naver.com>
2023-07-01 16:00:11 +0900

    ci(lint-staged.config): staged에 올라온 파일만 check하기 위해config 변경
    기존 npm run check-types는 trace되고 있는 모든 파일에 대해서 검사 진행하기 때문에 commit에 해당하는 staged 파일만 검사 할 수 있도록 변경

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-  '**/*.ts?(x)': () => 'npm run check-types',
+  '**/*.ts?(x)': (filenames) => [
+    ...filenames.map((filename) => `eslint --fix '${filename}'`),
+    ...filenames.map(
+      (filename) => `bash -c tsc --noEmit --pretty '${filename}'`
+    ),
+  ],
   '*.{json,yaml}': ['prettier --write'],
 };
