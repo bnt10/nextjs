535d3b4743cd70b92db277954037227b4bfda064
jin <bnt10@naver.com>
2023-07-08 12:16:25 +0900

    feat: add skip button functionality to splash page
    splahs page의 skip을 위한 버튼 추가
    
    feat #1

--- src/component/intro/wraper.tsx
+++ src/component/intro/wraper.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 import Button from '../common/Button'
 import type { IntroProps } from './type'
@@ -6,21 +7,31 @@ import type { IntroProps } from './type'
 export default function IntroWraper({
   onNext,
   onPrev,
+
   DisableNextButton = false,
   children,
 }: IntroProps) {
+  const router = useRouter()
+
   const preButtonSt = {
     button: 'mr-auto text-left text-base uppercase text-white/[0.44]',
   }
   const nextButtonSt = {
     button: 'ml-auto text-left text-base uppercase text-white',
   }
+  const skipButtonSt = {
+    button:
+      'top-[14pxr] font-normal uppercase leading-normal text-[16pxr] text-white text-opacity-40',
+  }
+
+  const onSkip = () => {
+    router.push('/')
+  }
+
   return (
     <div className="h-screen bg-gray-900 px-24pxr pt-14pxr">
       {!DisableNextButton && (
-        <div className="top-[14pxr] font-normal uppercase leading-normal text-[16pxr] text-white text-opacity-40">
-          Skip
-        </div>
+        <Button title={'Skip'} style={skipButtonSt} handler={onSkip} />
       )}
 
       <motion.div
