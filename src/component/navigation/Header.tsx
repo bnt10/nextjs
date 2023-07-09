59eacbe0650ed37515c5ce58fd5a455e8ca229c2
jin <bnt10@naver.com>
2023-07-09 15:04:36 +0900

    feat: add header profile button and index page image
    header profile button 및 index page image  추가
    
    feat #1

--- src/component/navigation/Header.tsx
+++ src/component/navigation/Header.tsx
@@ -3,19 +3,30 @@ import Button from '../common/Button'
 export default function Header() {
   const headerBtnSt = {
     button: 'p-9pxr',
-    icon: 'w-24pxr, h-24pxr relative',
+    icon: 'w-24pxr h-24pxr relative',
+  }
+  const profileSt = {
+    button: '',
+    icon: 'w-42pxr h-42pxr relative rounded-full',
   }
   return (
-    <div className="mx-auto mt-13pxr flex h-42pxr w-327pxr items-center justify-between bg-slate-500">
+    <div className="mx-auto mt-13pxr flex h-42pxr w-327pxr items-center justify-between ">
       <Button
         icon={'/assets/images/todo/header/more-button.svg'}
         style={headerBtnSt}
         title={''}
         handler={() => {}}
       />
-      <span className="text-center text-xl text-white/[0.87]">Index</span>
+      <span className="text-center  text-xl leading-5 tracking-[-0.03125rem] text-white/[0.87]">
+        Index
+      </span>
 
-      <div>profile</div>
+      <Button
+        icon={'/assets/images/todo/header/profile.jpg'}
+        style={profileSt}
+        title={''}
+        handler={() => {}}
+      />
     </div>
   )
 }
--- src/pages/todo/home/index.tsx
+++ src/pages/todo/home/index.tsx
@@ -1,9 +1,30 @@
+import Image from 'next/image'
+
 import HomeLayout from '@/layouts/todo/HomeLayout'
 
 export default function TodoHome() {
   return (
     <>
-      <HomeLayout>TodoHome home</HomeLayout>
+      <HomeLayout>
+        <div className="mt-75pxr flex flex-col items-center justify-center">
+          <div className="items-start">
+            <Image
+              src={'/assets/images/todo/home/index.svg'}
+              alt="Image description"
+              width={227}
+              height={227}
+            />
+          </div>
+          <div className="relative mt-[10pxr]">
+            <div className=" text-xl leading-normal  text-white text-opacity-90">
+              What do you want to do today?
+            </div>
+            <div className="mt-10pxr text-center text-base font-normal leading-normal text-white text-opacity-90">
+              Tap + to add your tasks
+            </div>
+          </div>
+        </div>
+      </HomeLayout>
     </>
   )
 }
