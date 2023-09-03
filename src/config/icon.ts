cb7bb6ac4f5d030c66e98e3e9647673964432555
jin <bnt10@naver.com>
2023-09-03 13:14:08 +0900

    refactor: add icon path constants

---
+++ src/config/icon.ts
@@ -0,0 +1,18 @@
+export const ICON_CLOCK = '/assets/images/todo/footer/clock.svg'
+export const ICON_PROFILE = '/assets/images/todo/footer/profile.svg'
+export const ICON_HEADER_PROFILE = '/assets/images/todo/header/profile.jpg'
+export const ICON_ADD = '/assets/images/todo/footer/add.svg'
+export const ICON_CALENDAR = '/assets/images/todo/footer/calendar.svg'
+export const ICON_HOME = '/assets/images/todo/footer/home.svg'
+export const ICON_MORE = '/assets/images/todo/header/more-button.svg'
+
+export const ICON_TIMER = '/assets/images/todo/home/timer.svg'
+export const ICON_TAG = '/assets/images/todo/home/tag.svg'
+export const ICON_FALG = '/assets/images/todo/home/flag.svg'
+export const ICON_SEND = '/assets/images/todo/home/send.svg'
+
+export const ICON_PRIORITY = '/assets/images/todo/modal/priority.svg'
+export const ICON_EMPTY = '/assets/images/todo/home/index.svg'
+
+export const ICON_CLOSE = '/assets/images/todo/editor/close.svg'
+export const ICON_REPEAT = '/assets/images/todo/editor/repeat.svg'
