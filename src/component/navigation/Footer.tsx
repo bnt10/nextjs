f3ff957553ced8c868f064e93a4205fe1b39027f
bnt10 <bnt10@naver.com>
2023-09-25 21:45:04 +0900

    refactor: move modal code to footer

--- src/component/navigation/Footer.tsx
+++ src/component/navigation/Footer.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 import {
   ICON_ADD,
@@ -7,10 +8,46 @@ import {
   ICON_HOME,
   ICON_PROFILE,
 } from '@/config/icon'
+import type { FormSchema } from '@/hooks/type'
+import { useForm } from '@/hooks/useForm'
 import useModal from '@/hooks/useModal'
+import { addTaskDescription, addTaskInputSt } from '@/styles/todo/home'
 
 import Button from '../common/Button'
+import Input from '../common/Input'
+import Modal from '../common/Modal'
+import TaskButton from '../todo/home/TasskButton'
 
+type TaskFormElements = 'title' | 'description'
+
+const addTaskShcema: FormSchema<TaskFormElements> = {
+  title: {
+    value: '',
+    type: 'text',
+    isControlled: true,
+    name: 'title',
+    placeholder: 'title',
+    validate: (value: string) => {
+      if (!value) {
+        return '입력이 필요합니다.'
+      }
+      return null
+    },
+  },
+  description: {
+    value: '',
+    type: 'text',
+    isControlled: true,
+    name: 'description',
+    placeholder: 'description',
+    validate: (value: string) => {
+      if (!value) {
+        return '입력이 필요합니다.'
+      }
+      return null
+    },
+  },
+}
 export default function Footer() {
   const { openModal, setOpenModal } = useModal()
   const router = useRouter()
@@ -61,20 +98,65 @@ export default function Footer() {
       style: textWithIconBtnStyle,
     },
   ]
+  const { handleOnChange, handleOnSubmit, getFormFields } =
+    useForm(addTaskShcema)
+
+  const addTaskSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+  const { title, description } = getFormFields()
+
+  useEffect(() => {
+    if (openModal) {
+      title.ref?.current?.focus()
+    }
+  }, [openModal])
 
   return (
     <div className="absolute bottom-0pxr box-border flex h-100pxr  w-full justify-between bg-footer-gray px-20pxr pt-12pxr">
-      {footerItem.map(({ title, icon, style, handler }) => {
+      {footerItem.map(({ title: footerItemTitle, icon, style, handler }) => {
         return (
           <Button
-            key={title}
-            title={title}
+            key={footerItemTitle}
+            title={footerItemTitle}
             handler={handler}
             icon={icon}
             style={style}
           />
         )
       })}
+      {openModal && (
+        <Modal>
+          <div className="absolute bottom-[0] h-228pxr w-393pxr flex-col items-center justify-center rounded-t-2xl bg-footer-gray px-25pxr pb-17pxr pt-25pxr">
+            <form onSubmit={handleOnSubmit(addTaskSubmit)}>
+              <p className="mb-14pxr text-left text-xl font-bold text-white/[0.87]">
+                Add Task
+              </p>
+
+              <Input
+                type={title.type}
+                value={title.value}
+                inputRef={title.ref}
+                name={title.name}
+                placeholder={title.placeholder}
+                handleInputChange={handleOnChange}
+                style={addTaskInputSt}
+              />
+              <Input
+                type={description.type}
+                value={description.value}
+                inputRef={description.ref}
+                name={description.name}
+                placeholder={description.placeholder}
+                handleInputChange={handleOnChange}
+                style={addTaskDescription}
+              />
+
+              <TaskButton />
+            </form>
+          </div>
+        </Modal>
+      )}
     </div>
   )
 }
--- src/pages/todo/home/index.tsx
+++ src/pages/todo/home/index.tsx
@@ -1,64 +1,10 @@
-import { useEffect } from 'react'
-
-import Input from '@/component/common/Input'
-import Modal from '@/component/common/Modal'
 import EmptyTodoList from '@/component/todo/home/EmptyTodoList'
 import SortButton from '@/component/todo/home/SortButton'
-import TaskButton from '@/component/todo/home/TasskButton'
 import TodoList from '@/component/todo/home/TodoList'
 import TodoSearchBar from '@/component/todo/home/TodoSearchBar'
-import type { FormSchema } from '@/hooks/type'
-import { useForm } from '@/hooks/useForm'
-import useModal from '@/hooks/useModal'
 import HomeLayout from '@/layouts/todo/HomeLayout'
-import { addTaskDescription, addTaskInputSt } from '@/styles/todo/home'
-
-type TaskFormElements = 'title' | 'description'
-
-const addTaskShcema: FormSchema<TaskFormElements> = {
-  title: {
-    value: '',
-    type: 'text',
-    isControlled: true,
-    name: 'title',
-    placeholder: 'title',
-    validate: (value: string) => {
-      if (!value) {
-        return '입력이 필요합니다.'
-      }
-      return null
-    },
-  },
-  description: {
-    value: '',
-    type: 'text',
-    isControlled: true,
-    name: 'description',
-    placeholder: 'description',
-    validate: (value: string) => {
-      if (!value) {
-        return '입력이 필요합니다.'
-      }
-      return null
-    },
-  },
-}
 
 export default function TodoHome() {
-  const { openModal } = useModal()
-  const { handleOnChange, handleOnSubmit, getFormFields } =
-    useForm(addTaskShcema)
-
-  const addTaskSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-  }
-  const { title, description } = getFormFields()
-
-  useEffect(() => {
-    if (openModal) {
-      title.ref?.current?.focus()
-    }
-  }, [openModal])
   const isEmptyTodoList = false
   return (
     <>
@@ -67,39 +13,6 @@ export default function TodoHome() {
           <TodoSearchBar />
           <SortButton title={'Today'} />
           {isEmptyTodoList ? <EmptyTodoList /> : <TodoList />}
-
-          {openModal && (
-            <Modal>
-              <div className="absolute bottom-[0] h-228pxr w-393pxr flex-col items-center justify-center rounded-t-2xl bg-footer-gray px-25pxr pb-17pxr pt-25pxr">
-                <form onSubmit={handleOnSubmit(addTaskSubmit)}>
-                  <p className="mb-14pxr text-left text-xl font-bold text-white/[0.87]">
-                    Add Task
-                  </p>
-
-                  <Input
-                    type={title.type}
-                    value={title.value}
-                    inputRef={title.ref}
-                    name={title.name}
-                    placeholder={title.placeholder}
-                    handleInputChange={handleOnChange}
-                    style={addTaskInputSt}
-                  />
-                  <Input
-                    type={description.type}
-                    value={description.value}
-                    inputRef={description.ref}
-                    name={description.name}
-                    placeholder={description.placeholder}
-                    handleInputChange={handleOnChange}
-                    style={addTaskDescription}
-                  />
-
-                  <TaskButton />
-                </form>
-              </div>
-            </Modal>
-          )}
         </div>
       </HomeLayout>
     </>
