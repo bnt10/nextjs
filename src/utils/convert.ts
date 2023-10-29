279801eee07051d3002099bef39d40552754e369
jin <bnt10@naver.com>
2023-10-29 13:53:13 +0900

    refactor: remove unnessary util

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,7 +1,5 @@
 import moment from 'moment-timezone'
 
-import type { DateData, TimeData } from '@/types/schedule'
-
 export const newDate = (dateTimeString: string) => {
   const utcDateTime = moment
     .tz(
@@ -18,29 +16,4 @@ export function currentDate() {
   const momentObject = moment(currentTime, 'YYYY-MM-DD hh:mm A')
 
   return momentObject.toDate()
-}
-
-export function combineDateAndTime(
-  date: DateData,
-  time: TimeData
-): moment.Moment {
-  const { hour, minute, amPm } = time
-  const { year, month, day } = date
-
-  const calculatedHour =
-    amPm?.toUpperCase() === 'PM'
-      ? parseInt(hour ?? '0', 10) + 12
-      : parseInt(hour ?? '0', 10)
-  const calculatedMinute = parseInt(minute ?? '0', 10)
-  const dateTimeString = `${year}-${month}-${day} ${calculatedHour}:${calculatedMinute}`
-
-  const utcDateTime = moment
-    .tz(
-      dateTimeString,
-      'YYYY-MM-DD hh:mm',
-      process.env.TIMEZONE || 'Asia/Seoul'
-    )
-    .utc()
-
-  return utcDateTime
 }
