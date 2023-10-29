4b444790d7d06dcc30145f0396da4f61abfae67c
jin <bnt10@naver.com>
2023-10-29 13:27:09 +0900

    refactor: remove unnecessary util

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,49 +1,6 @@
 import moment from 'moment-timezone'
 
-import type { DateData, TimeData, TimeZone } from '@/types/schedule'
-
-export const convertToUTC = (
-  time: TimeData,
-  date: DateData,
-  timeZone: TimeZone
-) => {
-  const { hour, minute, amPm } = time
-  const { year, month, day } = date
-
-  const formattedTime = `${hour}:${minute} ${amPm}`
-  const formattedDate = `${year}-${month}-${day}`
-
-  const dateTimeString = `${formattedDate} ${formattedTime}`
-
-  const utcDateTime = moment
-    .tz(dateTimeString, 'YYYY-MM-DD hh:mm A', timeZone)
-    .utc()
-
-  return utcDateTime
-}
-
-export const convertFromUTC = (
-  utcDateTime: moment.Moment,
-  timeZone?: TimeZone
-): { time: TimeData; date: DateData } => {
-  const localDateTime = utcDateTime
-    .clone()
-    .tz(timeZone || process.env.TIMEZONE || 'Asia/Seoul')
-
-  const time: TimeData = {
-    hour: localDateTime.format('hh'),
-    minute: localDateTime.format('mm'),
-    amPm: localDateTime.format('A'),
-  }
-
-  const date: DateData = {
-    year: localDateTime.year(),
-    month: localDateTime.month() + 1,
-    day: localDateTime.date(),
-  }
-
-  return { time, date }
-}
+import type { DateData, TimeData } from '@/types/schedule'
 
 export const newDate = (dateTimeString: string) => {
   const utcDateTime = moment
@@ -67,15 +24,23 @@ export function combineDateAndTime(
   date: DateData,
   time: TimeData
 ): moment.Moment {
-  let dateTime = moment(`${date.year}-${date.month}-${date.day}`, 'YYYY-MM-DD')
+  const { hour, minute, amPm } = time
+  const { year, month, day } = date
 
-  dateTime = dateTime.set({
-    hour:
-      time.amPm?.toUpperCase() === 'PM'
-        ? parseInt(time.hour ?? '0', 10) + 12
-        : parseInt(time.hour ?? '0', 10),
-    minute: parseInt(time.minute ?? '0', 10),
-  })
+  const calculatedHour =
+    amPm?.toUpperCase() === 'PM'
+      ? parseInt(hour ?? '0', 10) + 12
+      : parseInt(hour ?? '0', 10)
+  const calculatedMinute = parseInt(minute ?? '0', 10)
+  const dateTimeString = `${year}-${month}-${day} ${calculatedHour}:${calculatedMinute}`
 
-  return dateTime
+  const utcDateTime = moment
+    .tz(
+      dateTimeString,
+      'YYYY-MM-DD hh:mm',
+      process.env.TIMEZONE || 'Asia/Seoul'
+    )
+    .utc()
+
+  return utcDateTime
 }
