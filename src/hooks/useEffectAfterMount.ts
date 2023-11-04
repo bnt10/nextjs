211a3ba4b9bd93b798a720498a45f1d23e8afd0d
jin <bnt10@naver.com>
2023-11-04 19:18:14 +0900

    feat: add Custom Hook to Skip Initial Rendering
     In this commit, I've introduced a custom hook
    designed not to execute during the initial
    rendering phase but to rely solely on
    dependencies for updates.

--- src/hooks/useEffectAfterMount.ts
+++ src/hooks/useEffectAfterMount.ts
@@ -3,14 +3,7 @@ import { useEffect, useRef } from 'react'
 
 function useEffectAfterMount(cb: EffectCallback, dependencies: DependencyList) {
   const justMounted = useRef(true)
-  const dependenciesRef = useRef(dependencies)
-  console.log(
-    justMounted.current,
-    'zz',
-    dependenciesRef.current === dependencies,
-    dependenciesRef.current,
-    dependencies
-  )
+
   // eslint-disable-next-line consistent-return
   useEffect(() => {
     if (!justMounted.current) {
